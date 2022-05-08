6edac816d3c195b36aa4eae8c8d66811bf337090
Nabil Naja <nabilnaja9@gmail.com>
2022-05-08 19:11:36 +0200

    Time: 4074 ms (45.58%), Space: 16.3 MB (5.72%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.py
@@ -0,0 +1,23 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        """
+        Sub problems : dp(i) LIS from index i till 0
+        
+        Original problem : dp(n)
+        Relate : dp(i) = max(
+                
+            )
+        Base :  dp(0) = 1
+        Topological order : n, n-1, .. , 0
+        Time : O(n) * O(1)
+        """
+        n = len(nums)
+        @cache
+        def dp(i = n - 1):
+            if i == 0:
+                return 1
+            return max([dp(j) for j in range (i) if nums[j] < nums[i]], default=0) + 1
+        return max(dp(i) 
+                   for i in range(len(nums)))
+                    
+        
