74cadb4f6ed54f37afbb383ff6ecd2b2a581a074
Nabil Naja <nabilnaja9@gmail.com>
2022-05-08 17:46:18 +0200

    Time: 74 ms (19.03%), Space: 14 MB (46.62%) - LeetHub

--- 139-word-break/139-word-break.py
+++ 139-word-break/139-word-break.py
@@ -9,5 +9,14 @@ class Solution:
                 if s[i:end] in wordDict and dp(end):
                     return True
             return False
+        #return dp()
+        def dp():
+            memo = [False] * ( n + 1)
+            memo[0] =  True
+            for i in range(1, len(memo)):
+                for j in range(i):
+                    if memo[j] and s[j:i] in wordDict:
+                        memo[i] = True
+                        break
+            return memo[n]
         return dp()
-                    
