6598886dc68d6c6d417f140de6e10048bfb36de0
Nabil Naja <nabilnaja9@gmail.com>
2022-05-09 19:04:10 +0200

    Time: 372 ms (11.06%), Space: 24 MB (34.62%) - LeetHub

--- 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.py
@@ -1,4 +1,18 @@
 class Solution:
+    """
+        Sub problems : dp(i,t,hold) max profit from index 0 till i using t transaction
+                        hold mean we are holding stock
+        
+        Original problem : dp(0,k,0)
+        Relate : dp(i,t,hold) = 
+            if hold -> max(dp(i + 1, t -1 , 0) + prices[i], dp(i + 1, t, 1) )
+            else ->   max(dp(i + 1, t , 1) - prices[i], dp(i + 1, t, 0) )
+        Base :  t = 0 return 0
+                i = n return 0
+        Topological order : 0 , 1 , n; t , t-1 , 0
+        Time : O(n*k)
+        Spac
+    """
     def maxProfit(self, k: int, prices: List[int]) -> int:
         n = len(prices)
         memo = [[[0] * 2 for _ in range(k + 1)] for __ in range(n + 1)]
