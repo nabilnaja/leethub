14c826f97f2b4a6bdc7d124c5fcb470dc8e597c8
Nabil Naja <nabilnaja9@gmail.com>
2022-05-14 19:13:29 +0200

    Time: 54 ms (28.48%), Space: 14 MB (45.31%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -9,21 +9,34 @@ class Solution:
         Time : 
         """
         n = len(s)
-        def is_valid(s):
+        def is_valid_rec(s):
             return True if int(s) <= 26 else False
+        def is_valid_iter(s):
+            return True if 10 <= int(s) <= 26 else False
         
         @cache
         def dp(i = 0):
-            
             if i == n:
                 return 1
             if s[i] == '0':
                 return 0
             if i == n - 1:
                 return 1
-            
             result = dp(i+1)
-            return dp(i+2) + result if is_valid(s[i : i + 2]) else result
+            return dp(i+2) + result if is_valid_rec(s[i : i + 2]) else result
+        
+        def dp():
+            
+            memo = [0 for _ in range(n + 1)]
+            memo[-1] = 1
+            if s[n-1] != '0':
+                memo[n-1] = 1
+            
+            for i in range(len(memo) - 3, -1, -1):
+                if s[i] != '0':
+                    result = memo[i+1]
+                    memo[i] = memo[i+2] + result if is_valid_iter(s[i : i + 2]) else result
+            return memo[0]
         
         return dp()
             
