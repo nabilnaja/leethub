9d014d8e55a003a8d66e663e377d1ea322746a42
Nabil Naja <nabilnaja9@gmail.com>
2022-05-14 19:37:46 +0200

    Time: 51 ms (34.88%), Space: 13.9 MB (45.31%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -4,9 +4,9 @@ class Solution:
         Sub problems : dp(i) number of ways from i, i-1 .. until 0 
         Original problem : dp(n)
         Relate : dp(i) = dp(i-1) + 1 + anther 1 if s[i-1, i] is valid
-        Base :  dp(0) = 0 if s[0] == 0 else 1         
+        Base :  i = n : 1,  s[i] = '0' : 0,  i == n - 1: 1  
         Topological order : 0 , 1, ..., n
-        Time : 
+        Time : O(n)
         """
         n = len(s)
         def is_valid_rec(s):
