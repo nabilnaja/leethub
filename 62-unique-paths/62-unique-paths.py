71151137cfd789fc5285e0735fc534cf27ecee34
Nabil Naja <nabilnaja9@gmail.com>
2022-05-15 17:28:51 +0200

    Time: 49 ms (36.11%), Space: 13.9 MB (75.16%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -8,5 +8,16 @@ class Solution:
                 return 0
             
             return dp(i, j+1) + dp(i+1, j)
+        
+        def dp():
+            memo = [[1] * n for _ in range(m)]
+
+            for i in range(m - 2, -1, -1):
+                for j in range(n - 2, -1, -1):
+                    memo[i][j] = memo[i + 1][j] + memo[i][j + 1]
+
+            return memo[0][0]
+                    
+                    
         return dp()
         
