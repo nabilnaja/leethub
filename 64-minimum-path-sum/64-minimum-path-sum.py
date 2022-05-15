d43cbea39137f9786323f977876ec4381d372e88
Nabil Naja <nabilnaja9@gmail.com>
2022-05-15 20:05:28 +0200

    Time: 143 ms (45.24%), Space: 15.8 MB (30.92%) - LeetHub

--- 64-minimum-path-sum/64-minimum-path-sum.py
+++ 64-minimum-path-sum/64-minimum-path-sum.py
@@ -12,4 +12,20 @@ class Solution:
             
             return min(dp(i+1,j), dp(i, j+ 1)) + grid[i][j]
         
+        def dp():
+            
+            memo = [[0] * (n) for _ in range(m)]
+            memo[m-1][n-1] = grid[m-1][n-1] 
+            
+            for j in range(n - 2, - 1, -1):
+                memo[m-1][j] = grid[m-1][j] + memo[m-1][j + 1]
+                
+            for i in range(m - 2, -1 , -1):
+                memo[i][n-1] = grid[i][n-1] + memo[i + 1][n-1]
+
+            for i in range (m - 2, -1, -1):
+                for j in range (n - 2, -1, -1):
+                    memo[i][j] = min(memo[i+1][j], memo[i][j+ 1]) + grid[i][j]
+
+            return memo[0][0]
         return dp()
