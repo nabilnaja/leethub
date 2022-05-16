04fd4f1e357e553c8a03e3fd33cae655e3e2e6a2
Nabil Naja <nabilnaja9@gmail.com>
2022-05-16 10:49:42 +0200

    Time: 60 ms (45.47%), Space: 13.9 MB (75.13%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -10,5 +10,24 @@ class Solution:
             
             
             return dp(i, j+1) + dp(i+1, j)
+        
+        def dp():
+            memo = [[0] * n for _ in range(m)]
+              
+            memo[m-1][n-1] = 0 if obstacleGrid[m-1][n-1] else 1
+            
+            for i in range(n-2, -1,-1):
+                memo[m-1][i] = 0 if obstacleGrid[m-1][i] or not memo[m-1][i+1]  else 1 
+            for j in range(m-2, -1,-1):
+                memo[j][n-1] = 0 if obstacleGrid[j][n-1] or not memo[j+1][n-1] else 1 
+
+            for i in range(m - 2, -1, -1):
+                for j in range(n - 2, -1, -1):
+                    if obstacleGrid[i][j] == 1 :
+                        memo[i][j] = 0
+                    else:
+                        memo[i][j] = memo[i + 1][j] + memo[i][j + 1]
+
+            return memo[0][0]
         return dp()
         
