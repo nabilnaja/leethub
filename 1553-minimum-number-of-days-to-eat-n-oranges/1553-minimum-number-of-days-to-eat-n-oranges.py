f4eda4e828861b036d24dbc13753a6356c099c4f
Nabil Naja <nabilnaja9@gmail.com>
2022-05-17 20:45:43 +0200

    Time: 86 ms (27.88%), Space: 14.6 MB (45.19%) - LeetHub

---
+++ 1553-minimum-number-of-days-to-eat-n-oranges/1553-minimum-number-of-days-to-eat-n-oranges.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minDays(self, n: int) -> int:
+        """
+        Sub problems : dp(i) min number of days to eat i orange.
+        Original problem : dp(0)
+        Relate : dp(i) = min(dp())
+        Base :  dp(n) = 0
+        Topological order : 0,1,2,..,n
+        Time : 
+        """
+        if n == 1:
+            return 1
+        @cache
+        def dp(i = n):
+            if i == 1:
+                return 1     
+            if i <= 0:
+                return 0 
+
+          
+            return 1 + min( (i%2) + dp(i//2), (i%3) + dp(i//3) ) 
+        
+        return dp()
+        
