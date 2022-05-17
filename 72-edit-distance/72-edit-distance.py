61c515f24567685ab0fdceb545abe270418b4c71
Nabil Naja <nabilnaja9@gmail.com>
2022-05-17 18:56:13 +0200

    Time: 333 ms (14.40%), Space: 17.6 MB (64.08%) - LeetHub

--- 72-edit-distance/72-edit-distance.py
+++ 72-edit-distance/72-edit-distance.py
@@ -11,13 +11,31 @@ class Solution:
             if j == n:
                 return m - i
             
-            result = []
-            same = dp(i+1, j+1) if word1[i] == word2[j] else dp(i+1, j+1) + 1
-            result.append(same)
-            result.append(dp(i+1, j)+ 1)
-            result.append(dp(i, j+1)+ 1)
-            return min(result)
-        
+            down_right = dp(i+1, j+1) if word1[i] == word2[j] else dp(i+1, j+1) + 1
+            down = dp(i+1, j) + 1
+            right = dp(i, j+1) + 1
+            return min(right,down , down_right)
+        def dp():
+            memo = [[0] * (n+1) for _ in range (m+1)]
+            memo_rows = len(memo)
+            memo_cols = len(memo[0])
+            
+            
+            
+            for i in range(memo_rows):
+                memo[i][memo_cols-1] = memo_rows - i - 1
+            
+            for j in range(memo_cols):
+                memo[memo_rows-1][j] = memo_cols - j - 1
+                
+            for i in range (memo_rows - 2, -1, -1):
+                for j in range (memo_cols - 2, -1, -1):
+                    right = memo[i][j+1] + 1
+                    down = memo[i+1][j] + 1
+                    down_right = memo[i+1][j+1] + 1 if word1[i] != word2[j] else memo[i+1][j+1]
+                    memo[i][j] = min(right, down, down_right)
+                    
+            return memo[0][0]
         return dp()
             
             
