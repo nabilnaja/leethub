f72224aa863f094e535230ccbfe8e37d92be36dc
Nabil Naja <nabilnaja9@gmail.com>
2022-05-18 18:32:49 +0200

    Time: 1754 ms (14.71%), Space: 23.5 MB (21.23%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -11,10 +11,17 @@ class Solution:
         Time : 
         """
         n = len(prices)
+        state_numbers = 2
         @cache
         def dp(i = 0, hold = 0):
             if i == n:
                 return 0
             return max(dp(i+1,1), dp(i+1,0) - fee + prices[i]) if hold else max(dp(i+1,1) - prices[i], dp(i+1,0))
+        def dp():
+            memo = [[0] * state_numbers for _ in range(n+1)]
+            for i in range(len(memo) - 2, -1, -1):
+                for hold in range(state_numbers):
+                    memo[i][hold] = max(memo[i+1][1], memo[i+1][0] - fee + prices[i]) if hold else max(memo[i+1][1] - prices[i], memo[i+1][0])
+            return memo[0][0]
         return dp()
         
