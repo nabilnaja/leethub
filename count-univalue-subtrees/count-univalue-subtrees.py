f54025a75fecbb35aa0b6fa6fe829865bd3bc407
Nabil Naja <nabilnaja9@gmail.com>
2022-05-21 18:13:34 +0200

    Time: 60 ms, Memory: 14 MB - LeetHub

---
+++ count-univalue-subtrees/count-univalue-subtrees.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        def helper(root, val):
+            if not root: 
+                return True
+            if not all([ helper(root.left, root.val), helper(root.right, root.val)]):
+                return False
+            self.count += 1
+            return root.val == val
+            
+        
+
+        if not root:
+            return 0
+        self.count = 0
+        helper(root, root.val)
+        return self.count
+        
