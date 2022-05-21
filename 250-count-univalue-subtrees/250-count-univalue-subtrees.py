ac3c0090cafd55b2856eb34ab202432a2ddb6ba3
Nabil Naja <nabilnaja9@gmail.com>
2022-05-21 18:04:24 +0200

    Time: 44 ms (62.69%), Space: 14 MB (90.79%) - LeetHub

--- 250-count-univalue-subtrees/250-count-univalue-subtrees.py
+++ 250-count-univalue-subtrees/250-count-univalue-subtrees.py
@@ -6,26 +6,17 @@
 #         self.right = right
 class Solution:
     def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
-        def helper(root):
-            if not root.left and not root.right:
-                self.count += 1
-                return True 
+        def helper(root, val):
+            if not root: 
+                return True
+            if not all([helper(root.left, root.val) , helper(root.right ,root.val)]):
+                return False
+            self.count += 1
+            return root.val == val
             
-            is_uni = True
-            
-            if root.left:
-                is_uni = helper(root.left) and is_uni and root.left.val == root.val
-            
-            if root.right:
-                is_uni = helper(root.right) and is_uni and root.right.val == root.val
-                        
-            self.count += is_uni
-            return is_uni
-        
-
         if not root:
             return 0
         self.count = 0
-        helper(root)
+        helper(root, root.val)
         return self.count
         
