8c731d45d50981aeaefbc7fe78dab8ca70ef04df
Nabil Naja <nabilnaja9@gmail.com>
2022-05-22 18:35:03 +0200

    Time: 1210 ms (5.52%), Space: 187.8 MB (5.06%) - LeetHub

---
+++ 647-palindromic-substrings/647-palindromic-substrings.py
@@ -0,0 +1,30 @@
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        """
+        Sub problems : dp(i) represent the number of palindromic substrings in s[:i].
+        Original problem : dp(n) , n = len(s)
+        Relate : dp(i) = dp(i-1) + []
+        Base : i = 0 return 1, i < 0 =return 0
+        Topological order : n , ..., 0
+        Time :
+        """
+        def is_palindrome(start, end):
+            while start < end:
+                if s[start] != s[end]:
+                    return False
+                start =+ 1
+                end =-  1
+            return True
+            
+        @cache    
+        def dp(i,j):
+            if i == j: 
+                return 1
+            if i == j + 1:
+                return s[i] == s[j]
+            return dp(i+1, j-1) and s[i] == s[j]
+        
+        
+        result = sum([dp(i,j) for i in range(len(s)) for j in range(i,len(s))])
+        return result
+        
