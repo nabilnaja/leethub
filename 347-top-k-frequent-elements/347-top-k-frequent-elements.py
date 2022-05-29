b8418371057b2127bf782cd388914fe7cac14703
Nabil Naja <nabilnaja9@gmail.com>
2022-05-29 13:48:52 +0200

    Time: 113 ms (76.80%), Space: 19.3 MB (20.88%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -3,10 +3,34 @@ class Solution:
         """
         Time complexity: O(n log k) 
         Space complexity:  O(n + k)
+        
+        Heap solution
+        """
         """
         if k == len(nums):
             return nums
         
         counter = Counter(nums)
         return heapq.nlargest(k, counter.keys(), key=counter.get)
+        
+        """
+        """
+        Time complexity: O(n) 
+        Space complexity:  O(n)
+        
+        Bucket sort solution
+        """
+        if k == len(nums):
+            return nums
+        counter = Counter(nums)
+        bucket = [[] for _ in range(len(nums) + 1)]
+        for n, c in counter.items():
+            bucket[c].append(n)
+        result = [] 
+        for i in range(len(bucket) - 1, -1, -1):
+            for num in bucket[i]:
+                result.append(num)
+                if len(result) == k:
+                    return result
+        
         
