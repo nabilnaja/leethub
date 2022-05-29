e94b7af8afb2e3ad57cbb6f4dbfa8c0940332421
Nabil Naja <nabilnaja9@gmail.com>
2022-05-29 12:20:18 +0200

    Time: 137 ms (52.98%), Space: 20 MB (9.31%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -2,7 +2,7 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         """
         Time complexity: O(n * k), n is the number of words, k is the length of the longuest word 
-        Space complexity:  O(n * k), n and k are the same as for the TS. this space is occupied by the hashmap
+        Space complexity:  O(n * k), n and k are the same as for the TS. this space is occupied by the           hashmap
         """
         a = 'a'
         counter_size = 27
