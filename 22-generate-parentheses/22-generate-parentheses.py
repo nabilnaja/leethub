f6f8f0f1e9dc89ae7c2c23155f94e6402dab2945
Nabil Naja <nabilnaja9@gmail.com>
2022-06-02 19:33:48 +0200

    Time: 57 ms (34.39%), Space: 14.1 MB (76.86%) - LeetHub

--- 22-generate-parentheses/22-generate-parentheses.py
+++ 22-generate-parentheses/22-generate-parentheses.py
@@ -2,9 +2,7 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         """
         Time complexity: O(4^^n / (n * n **1/2)) 
-        Space complexity:  O(4^^n / (n * n **1/2)) 
-        
-        Heap solution
+        Space complexity:  O(4^^n / (n * n **1/2))         
         """
         def backtrack(sol, s=[], leftSum=0, rightSum=0):
             if len(s) == n * 2:
