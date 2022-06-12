26870b84b3d5fc33df703eee5ed0d70f19c67635
Nabil Naja <nabilnaja9@gmail.com>
2022-06-12 17:08:14 +0200

    Time: 307 ms (5.04%), Space: 15.8 MB (59.85%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.py
+++ 42-trapping-rain-water/42-trapping-rain-water.py
@@ -4,20 +4,22 @@ class Solution:
         Time complexity: O(n)  
         Space complexity:  O(1)        
         """
-        l, r = 0, len(height) - 1
-        total = 0
-        leftMax, rightMax = height[l], height[r]
-        res = 0
-        while l < r:
-            if leftMax < rightMax:
-                l += 1
-                leftMax = max(leftMax, height[l])
-                res += leftMax - height[l]
-            else:
-                r -= 1
-                rightMax = max(rightMax, height[r])
-                res += rightMax - height[r]
-        return res        
+        max_left = [height[0] for _ in range(len(height))] 
+        max_right = [height[len(height) - 1] for _ in range(len(height))]
+        
+        for i in range(1, len(height)):
+            max_left[i] = height[i] if height[i] > max_left[i-1] else max_left[i - 1]   
+        for i in range(len(height) -2 ,-1, -1 ):
+            max_right[i] = height[i] if height[i] > max_right[i+1] else max_right[i + 1]   
+        
+        water_volume = 0
+        for i in range(1, len(height) - 1):
+            current_volume = min(max_left[i], max_right[i]) - height[i]
+            if current_volume > 0:
+                water_volume += current_volume
+        return water_volume
+            
+        
                     
                 
             
