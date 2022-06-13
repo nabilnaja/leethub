1b1217da116d7ea0ace3fc1fb05fb03beb7a35d0
Nabil Naja <nabilnaja9@gmail.com>
2022-06-13 18:01:14 +0200

    Time: 89 ms (6.42%), Space: 14 MB (99.39%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,5 +1,9 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        """
+        Time complexity: O(log n)  
+        Space complexity:  O(1)        
+        """
         l, r = 0, len(nums) - 1
         while l <= r:
             mid = l + (r - l) // 2
