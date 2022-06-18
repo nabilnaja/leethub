f996d6a6eee7dee9eef45ddee407d20f2fd14e09
Nabil Naja <nabilnaja9@gmail.com>
2022-06-18 14:20:21 +0200

    Time: 137 ms (84.06%), Space: 14.1 MB (19.40%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.py
@@ -1,7 +1,12 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        """
+        Time complexity: O(n)
+        Space complexity:  O(1)
+        """
         window_count = [0] * 26
         res = 0
+        max_f = 0
         l = 0
         ord_a = ord('A')
         
@@ -9,9 +14,9 @@ class Solution:
             
             ch = ord(s[r]) - ord_a
             window_count[ch] += 1
-            max_ch = max(window_count)
+            max_f = max(window_count[ch], max_f)
             window_size = r - l + 1
-            if window_size - max_ch > k:
+            if window_size - max_f > k:
                 window_count[ord(s[l])- ord_a] -= 1
                 l += 1   
             else: 
