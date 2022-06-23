756570ca0de4add197a900f3ce4a676e8550f278
Nabil Naja <nabilnaja9@gmail.com>
2022-06-23 18:28:53 +0200

    Time: 138 ms (41.93%), Space: 24 MB (38.47%) - LeetHub

--- 143-reorder-list/143-reorder-list.py
+++ 143-reorder-list/143-reorder-list.py
@@ -11,27 +11,23 @@ class Solution:
         """
         if not head:
             return head 
-        
-        def reverseLinkedList(head):
-            if not head or not head.next:
-                return head
-            tail = reverseLinkedList(head.next)
-            head.next.next = head
-            head.next = None
-            return tail
-        
        
         slow_pointer = fast_pointer = head
             
         while fast_pointer and fast_pointer.next:
             slow_pointer, fast_pointer = slow_pointer.next, fast_pointer.next.next
+        
+        prev, curr = None, slow_pointer
+        
+        while curr:
+            curr.next, prev, curr = prev, curr, curr.next
             
-        list1, list2 = head, reverseLinkedList(slow_pointer) 
-
+        list1, list2 = head, prev
+        
         while list2.next:
             list1.next, list1 = list2, list1.next
             list2.next, list2 = list1, list2.next
-        
+
         return head 
         
         
