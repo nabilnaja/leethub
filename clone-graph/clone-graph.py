710677b142726f772a01ebce92e3e1c03c7bb2f2
Nabil Naja <nabilnaja9@gmail.com>
2022-07-19 23:07:42 +0200

    Time: 55 ms, Memory: 14.4 MB - LeetHub

---
+++ clone-graph/clone-graph.py
@@ -0,0 +1,37 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        """
+        Time complexity: O(v + e)  
+        Space complexity:  O(v)        
+        """
+        node_cache = {}
+        a = []
+        def get_node(current_node):
+            if not current_node:
+                return None
+
+            if current_node in node_cache:
+                return node_cache[current_node]
+            
+            node_copy = Node(current_node.val)
+            node_cache[current_node] = node_copy
+            
+            for neighbor in current_node.neighbors:
+                node_copy.neighbors.append(get_node(neighbor))
+                
+            
+            return node_copy
+        
+        return get_node(node)
+            
+            
+            
+            
