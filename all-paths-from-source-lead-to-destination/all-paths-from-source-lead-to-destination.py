27dfc21643c8b1615340e659ae2b8b5e7c910aea
Nabil Naja <nabilnaja9@gmail.com>
2022-07-20 20:07:01 +0200

    Time: 563 ms, Memory: 19.9 MB - LeetHub

---
+++ all-paths-from-source-lead-to-destination/all-paths-from-source-lead-to-destination.py
@@ -0,0 +1,41 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        """
+        Time complexity: O(v)  
+        Space complexity:  O(v + e)        
+        """
+        adj = defaultdict(list)
+        
+        for s, d in edges:
+            adj[s].append(d)
+        
+        
+        visited = set()
+        processed = set()
+        
+        def dfs(node):
+            if node in processed:
+                return True
+            if node in visited:
+                return False
+            if node == destination:
+                return not adj[node]
+            visited.add(node)
+            neighbours = adj[node]
+            if not neighbours:
+                return False
+            for neighbour in neighbours:
+                if not dfs(neighbour):
+                    return False
+                else:
+                    processed.add(neighbour)
+            visited.remove(node)   
+            return True
+        return dfs(source)
+            
+            
+            
+                
+            
+        
+        
