413ee6eb33b9596fba4d2607d9d6c9e129f58b5d
Nabil Naja <nabilnaja9@gmail.com>
2022-07-20 19:41:18 +0200

    Time: 350 ms (72.94%), Space: 19.4 MB (70.14%) - LeetHub

---
+++ 1059-all-paths-from-source-lead-to-destination/1059-all-paths-from-source-lead-to-destination.py
@@ -0,0 +1,31 @@
+class Solution:
+    def leadsToDestination(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        adj = defaultdict(list)
+        for s, d in edges:
+            adj[s].append(d)
+        
+        
+        visited = set()
+        ended = set()
+        def dfs(node):
+            if node in visited:
+                return False
+            if node == destination:
+                return not adj[node]
+            visited.add(node)
+            neighbours = adj[node]
+            if not neighbours:
+                return False
+            for neighbour in neighbours:
+                if not dfs(neighbour):
+                    return False
+            visited.remove(node)   
+            return True
+        return dfs(source)
+            
+            
+            
+                
+            
+        
+        
